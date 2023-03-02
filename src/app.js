5cdd3b674cb2f23e9cffa6a989bfd97715710d64
israel bonilla <israel.bonillad@gmail.com>
2023-03-02 01:44:26 +0000

    excuse generator listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//si siempre se repite una parte como [Math.floor(Math.random() * 3) es mejor hacerla una función//
+const reload = document.getElementById("reload");
+reload.addEventListener("click", _ => {
+  location.reload();
+});
+function random_word(words) {
+  return words[Math.floor(Math.random() * 3)];
+}
 
+function generateexcuse() {
+  let excuses = {
+    guilt: ["My cat", "My sister", "My dad", "My cousin"],
+    action: ["eat", "destroy", "burn", "throw away"],
+    item: ["my homework", "my project", "my tesis", "my report"],
+    time: ["yesterday", "an hour ago", "this morning", "the past week"]
+  };
+
+  let Guilt = random_word(excuses.guilt);
+  let Action = random_word(excuses.action);
+  let Item = random_word(excuses.item);
+  let Time = random_word(excuses.time);
+
+  return Guilt + " " + Action + " " + Item + " " + Time + " ";
+}
+
+//Mejor crear un boton para generar una excusa en vez de recargar la pagina
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateexcuse();
 };
